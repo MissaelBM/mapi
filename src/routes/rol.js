26c7d0f0e96bfbc358216a6f17463a66c8e982e5
missael <alebermor999@gmail.com>
2025-05-29 08:53:22 -0600

    Correccion de rutas

--- src/routes/rol.js
+++ src/routes/rol.js
@@ -7,10 +7,10 @@ module.exports = (connection) => {
   const controller = rolController(connection);
 
   router.post('/rol', controller.rol);
-  router.get('/rol',authenticateToken(['Administrador']), controller.consultar);
+  router.get('/rol', controller.consultar);
   router.get('/rol/:id', controller.consultarId);
   router.patch('/rol/:id', controller.actualizarRol);
-  router.delete('/rol/:id',authenticateToken(['Administrador']), controller.eliminarRol);
+  router.delete('/rol/:id', controller.eliminarRol);
  
 
   return router;
